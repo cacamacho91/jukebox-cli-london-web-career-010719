de33acdc34eec3828b356bf3c2af0dcc3b0c60fe
dashing-frame-3501 <cacamacho91@gmail.com>
2018-12-05 23:05:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,25 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"," - help : displays this help message"," - list : displays a list of songs you can play"," - play : lets you choose a song to play", " - exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_request = gets.chomp()
+
+  songs.each_with_index do |song_title, index|
+    binding.pry
+    puts "Playing #{song_title}" if song_request == (index+1).to_s || song_request == song_title
+  end
+
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
+
+play(songs)
